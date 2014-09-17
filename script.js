ea406a3e63da875391d0baa4aa3ab1dc66e0089f
Julie De Lorenzo <delorenzo.julie@gmail.com>
2014-09-16 22:52:34 -0400

    moved the draw to a delegate

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
+var opacity = [];
+var timeout;
+
 function clearSquares() {
+	//clear grid
 	$(".grid").remove();
 	$("br").remove();
+
+	//clear opacity array
+	/*
+	while (opacity.length > 0) {
+		opacity.pop();
+	}*/
 }
 
 function createSquares(numSquares) {
 	var edgeLen = 1000/numSquares;
+	var count = 0;
 	for (var $i = 0; $i < numSquares; $i++) {
 		for (var $j = 0; $j < numSquares; $j++) {
-			$(".container").append("<div class='grid'></div>");
+			$(".container").append("<div class='grid' id='" + count + "'></div>");
+			count++;
+			opacity[count] = 0;
 		}
 		$(".container").append("<br/>");
 	}
@@ -23,8 +36,32 @@ $(document).ready(function() {
 		clearSquares($numSquares);
 		createSquares($numSquares);
 	});
-
-	$(".grid").mouseenter(function() {
-		$(this).css("background-color", "#a8a8a8");
-	});
 });
+
+$(document).on('mouseenter', '.grid', function() {
+	//opacity[this.id] += 0.10;
+	$(this).css("background-color", "#a8a8a8");
+});
+
+/*$(document).on('mousedown', '.grid', function() {
+	$(".grid").mouseenter(function() {
+			$(this).css("background-color", "#a8a8a8");
+	});
+	timeout = setInterval(function() {
+		$(".grid").mouseenter(function() {
+			$(this).css("background-color", "#a8a8a8");
+		});
+	}, 500);
+
+	return false;
+});
+
+$(document).mouseup(function() {
+	clearInterval(timeout);
+	return false;
+});
+
+$(document).on('mouseout', '.container', function() {
+	clearInterval(timeout);
+	return false;
+});*/
