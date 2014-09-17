ce28ec1148f305cfe4408bfbd732252804ff53bb
Julie De Lorenzo <delorenzo.julie@gmail.com>
2014-09-16 22:00:44 -0400

    added sketchpad functionality

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-$(document).ready(function() {
-	for (var $i = 1; $i <= 4; $i++) {
-		for (var $j = 1; $j <= 4; $j++) {
-			$(".container").append("<div class='grid' id='" + $i*$j + "'></div>");
+function clearSquares() {
+	$(".grid").remove();
+	$("br").remove();
+}
+
+function createSquares(numSquares) {
+	var edgeLen = 1000/numSquares;
+	for (var $i = 0; $i < numSquares; $i++) {
+		for (var $j = 0; $j < numSquares; $j++) {
+			$(".container").append("<div class='grid'></div>");
 		}
 		$(".container").append("<br/>");
 	}
+	$(".grid").width(edgeLen);		
+	$(".grid").height(edgeLen);
+}
 
-	$(".grid").mouseenter(function() {
-		$(this).css("background-color", "purple");
+$(document).ready(function() {
+	createSquares(64);
+
+	$("button").click(function() {
+		var $numSquares = prompt("How many squares per side do you want the new grid to be?");
+		clearSquares($numSquares);
+		createSquares($numSquares);
 	});
 
-	$(".grid").mouseleave(function() {
-		$(this).css("background-color", "white");
+	$(".grid").mouseenter(function() {
+		$(this).css("background-color", "#a8a8a8");
 	});
 });
